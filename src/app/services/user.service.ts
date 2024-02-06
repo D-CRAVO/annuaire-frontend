1684e7742048a329bb99974de7e173e8edf82bd9
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-06 17:02:12 +0100

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AnnuaireService } from './annuaire.service';
+import { catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,49 @@ export class UserService {
   constructor(private httpClient: HttpClient, private annuaireService: AnnuaireService) { }
 
   getUsers(){
-    return this.httpClient.get(this.annuaireService.API_URL+this.ENDPOINT_USERS);
+    return this.httpClient.get(this.annuaireService.API_URL + this.ENDPOINT_USERS);
   }
 
   getUserById(id: number){
-    return this.httpClient.get(this.annuaireService.API_URL+this.ENDPOINT_USER+`/${id}`);
+    return this.httpClient.get(this.annuaireService.API_URL + this.ENDPOINT_USER + `/${id}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    )
+  }
+
+  updateUser(user: any){
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+    return this.httpClient.put(this.annuaireService.API_URL, user, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    )
+  }
+
+  deleteUserById(id: number){
+    this.httpClient.delete(this.annuaireService.API_URL + this.ENDPOINT_USER + `/${id}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    )
+  }
+
+  addUser(user: any){
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+    return this.httpClient.post(this.annuaireService.API_URL, user, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    )
+  }
+
+  private log(response: any){
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any){
+    console.error(error);
+    return of(errorValue);
   }
 }
