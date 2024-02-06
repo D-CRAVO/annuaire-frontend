1702311c62442acbeb7fd0d8badacaa4e0d25ab1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-06 17:01:53 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { UserListComponent } from './components/user-list/user-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 const routes: Routes = [
   {path:'user/:id', component: UserDetailsComponent},
+  {path:'user/add', component: AddUserComponent},
   {path:'edit/user/:id', component: EditUserComponent},
   {path:'users', component: UserListComponent}
 ];
