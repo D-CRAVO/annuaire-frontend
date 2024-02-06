36cacc6e4232e23aac1317e0cf09d79cc07a9117
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-06 17:02:15 +0100

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -6,12 +6,20 @@ export class User{
     zipCode: string;
     city: string;
 
-    constructor(id: number, firstname: string, lastname: string, address: string, zipCode: string, city: string){
-        this.id = id
-        this.firstname = firstname
-        this.lastname = lastname
-        this.address = address
-        this.zipCode = zipCode
-        this.city = city
+    constructor(
+        id: number,
+        firstname: string = 'Pierre', 
+        lastname: string = 'Leroy', 
+        address: string = '789 boulevard des étoiles', 
+        zipCode: string = '13003', 
+        city: string = 'Marseille'
+        ){
+            this.id = id
+            this.firstname = firstname
+            this.lastname = lastname
+            this.address = address
+            this.zipCode = zipCode
+            this.city = city
     }
+    
 }
