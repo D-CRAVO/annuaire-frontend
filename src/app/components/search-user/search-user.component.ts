3a333b2fec1e4821af091f48934bcb2807259c34
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-07 16:59:03 +0100

    Create search-user

--- src/app/components/search-user/search-user.component.ts
+++ src/app/components/search-user/search-user.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-search-user',
   templateUrl: './search-user.component.html',
   styleUrl: './search-user.component.css'
 })
-export class SearchUserComponent {
+export class SearchUserComponent implements OnInit{
+
+  searchTerms = new Subject<string>();
+  users$ : any;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ){}
+
+  ngOnInit(): void {
+      this.users$ = this.searchTerms.pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((term)=> this.userService.searchUserList(term))
+      )
+  }
+
+  search(term: string){
+    this.searchTerms.next(term)
+  }
 
   goToUserDetails(user: any){
     const link = ['/user', user.id]
