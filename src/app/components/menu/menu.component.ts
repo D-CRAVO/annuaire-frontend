457d713949862481445d4764d519e9d19fa2b56c
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-21 09:02:42 +0100

    Update menu.component.ts

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
